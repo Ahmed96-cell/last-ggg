4b28155f8a231bd8e603a175a2e1ccaa6dfd8e92
Ahmed Yahia El-asmer <yahiaahmed017@gmail.com>
2023-07-09 02:58:35 +0200

    fff

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ import "../src/index.css";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename="last-ggg">
+    <BrowserRouter basename="/last-ggg">
       <Provider store={store}>
         <App />
       </Provider>
